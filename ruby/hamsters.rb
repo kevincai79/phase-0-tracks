c368e4f8ce8bc542d1393a764e457e9ca06fcc3e
Eleanor Roosevelt <kevincai@YUANWENs-MacBook-Pro.local>
2017-04-17 22:18:40 -0700

    update code to convert user input to proper data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,13 +2,23 @@ def hamster_data
   puts "What's the hamster's name?"
   hamster_name = gets.chomp
   puts "How loud is this hamster (level 1 - 10)?"
-  volume = gets.chomp
+  volume = gets.chomp.to_f
   puts "What's its fur color?"
   fur_color = gets.chomp
-  puts "Is it a good candidate for adoption?"
-  adoptable = gets.chomp
+  adoptable = ""
+  loop do
+    puts "Is it a good candidate for adoption (y/n)?"
+    adoptable = gets.chomp
+    if adoptable == "y"
+      adoptable = true
+      break
+    elsif adoptable == "n"
+      adoptable = false
+      break
+    end
+  end
   puts "Estimated age?"
-  estimated_age = gets.chomp
+  estimated_age = gets.chomp.to_i
   if estimated_age == ""
     estimated_age = nil
   end
@@ -18,4 +28,5 @@ def hamster_data
   puts "Adoptable: #{adoptable}"
   puts "Estimated Age: #{estimated_age}"
 end
+
 hamster_data
