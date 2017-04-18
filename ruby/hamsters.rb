14ad352c6936e4a0134ed89a3f44b4e579b43c5a
Eleanor Roosevelt <kevincai@YUANWENs-MacBook-Pro.local>
2017-04-17 20:50:12 -0700

    add more code to hamsters.rb file

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -5,7 +5,17 @@ def hamster_data
   volume = gets.chomp
   puts "What's its fur color?"
   fur_color = gets.chomp
-  puts hamster_name, volume, fur_color
+  puts "Is it a good candidate for adoption?"
+  adoptable = gets.chomp
+  puts "Estimated age?"
+  estimated_age = gets.chomp
+  if estimated_age == ""
+    estimated_age = nil
+  end
+  puts "Hamster Name: #{hamster_name}"
+  puts "Volume: #{volume}"
+  puts "Fur Color: #{fur_color}"
+  puts "Adoptable: #{adoptable}"
+  puts "Estimated Age: #{estimated_age}"
 end
-
 hamster_data
