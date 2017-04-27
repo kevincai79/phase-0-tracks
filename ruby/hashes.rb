65a4d9aa34f1a7310b02fff08cfaf9076d2a95b3
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-04-27 00:05:14 -0700

    complete the code for entering client's data

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,4 @@
-# CLINET DATA
+# CLINET DATA FOR INTERIOR DESIGN
 
 # Set up an empty hash
 
@@ -11,3 +11,43 @@
 # -- Otherwise prompt the designer/user to input item for update
 
 # Print the latest version of hash
+
+client_data = Hash.new
+
+puts "What is the client's name?"
+client_data[:name] = gets.chomp
+
+puts "How old is the client?"
+client_data[:age] = gets.chomp.to_i
+
+puts "How many kids does the client have?"
+client_data[:children_num] = gets.chomp.to_i
+
+puts "What is the decor theme client wants?"
+client_data[:deco_theme] = gets.chomp
+
+puts "Does the client have cats (y/n)?"
+client_data[:have_cat] = gets.chomp.downcase
+if client_data[:have_cat] == "y"
+  client_data[:have_cat] = true
+else
+  client_data[:have_cat] = false
+end
+
+puts "Is there anything you want to update?"
+update_item = gets.chomp
+
+if update_item != "none"
+  puts "Please enter the new info for the update: #{update_item}?"
+  client_data[update_item.to_sym] = gets.chomp
+end
+
+if update_item == "have_cat"
+  if client_data[update_item.to_sym] == "y"
+    client_data[update_item.to_sym] = true
+  else
+  client_data[update_item.to_sym] = false
+ end
+end
+
+puts client_data
