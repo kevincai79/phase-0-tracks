a9ed685ba40bfd2ee06f98410ff6c26b299c52cd
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-04-26 22:21:12 -0700

    complete pseudocode for the program

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+# CLINET DATA
+
+# Set up an empty hash
+
+# Prompt the designer/user to input the data
+
+# Prompt the designer/user anything to update
+
+# -- If designer/user type in 'none', skip it
+
+# -- Otherwise prompt the designer/user to input item for update
+
+# Print the latest version of hash
