94cbb5e696f584b50f1e9e747f4b9e9e6161b613
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-04-28 23:59:22 -0700

    complete assignment 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,5 +1,8 @@
 
-# 5.6 release 0
+# 5.6 Mandatory Pairing: Solving Problems with Data Structures
+# Kevin Cai & Jaesley
+
+# 5.6 release 0: Implement a Simple Search
 
 def search_index(arr, x)
   i = 0
@@ -16,16 +19,41 @@ def search_index(arr, x)
 end
 end
 
-# 5.6 release 1
-
+# 5.6 release 1: Calculate Fibonacci Numbers
 
 def fib(x)
   fibo_number = [0, 1]
   (x - 2).times do |n|
   n = fibo_number[-1] + fibo_number[-2]
   fibo_number << n
-end
+  end
   puts fibo_number
 end
 
 fib(100)
+
+# 5.6 release 2: Sort an Array - BUBBLE SORT
+
+# Define a method and take an array as parameter
+
+=begin
+Loop through the array (array element - 1) times
+-- Check each element except the last element,if the element is greater than the element next to it, swap their positions
+=end
+
+# Continue above loop rules to check the array until not any element needs to be compared.
+
+def bubble_sort(arr)
+  loop do
+    sorted = false
+    (arr.length - 1).times do | i |
+  if arr[i] > arr[i+1]
+    arr[i], arr[i+1] = arr[i+1], arr[i]
+    sorted = true
+  end
+  end
+  break if sorted == false
+end
+arr
+end
+puts bubble_sort([1, 20, 3, -7, 100, 2, -15])
