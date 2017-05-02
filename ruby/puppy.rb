312e5b2b17a8a9b597022aa0c000a7d9771126dc
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-02 12:16:21 -0700

    add Puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+
+  PUPPY --------------------------
+  Breed
+
+  CHARACTERISTICS ------------------
+  Energy:
+  Size:
+  Color:
+  Personality: []
+
+  BEHAVIOR -------------------------
+  Bark:
+  Run:
+  Play:
+  Fetch:
+  Roll over:
+
+=end
