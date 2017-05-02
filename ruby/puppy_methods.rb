6c7cb5485885efd34053daf620eb0a77f9bfebde
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-02 15:10:28 -0700

    complete assignment 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,59 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times { puts 'Woof!'}
+  end
+
+  def roll_over
+    puts '*rolls over*'
+  end
+
+  def dog_years(int)
+    p int * 7
+  end
+
+  def jump(int)
+    puts "The dog just jumped #{int} times!"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
 end
+
+puppy = Puppy.new
+puppy.fetch('ball')
+puppy.speak(5)
+puppy.roll_over
+puppy.dog_years(5)
+puppy.jump(100)
+
+class Zoo
+
+  def initialize(animal, age)
+    @animal = animal
+    @age = age
+  end
+
+  def swim(int)
+    puts "#{@animal} can swim #{int} feets."
+  end
+
+  def eat(int)
+    puts "#{@animal} eats #{int} times a day."
+  end
+end
+
+zoo_animal = []
+50.times do
+  zoo_animal << Zoo.new("monkey", 2)
+end
+
+zoo_animal.each do |animal|
+  animal.swim(100)
+  animal.eat(3)
+end
+
 
