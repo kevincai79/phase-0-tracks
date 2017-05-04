485d146712e1d63664d1ff7786737b5d0456aa15
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-03 19:21:33 -0700

    add print method for printing list with good layout

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,12 +6,12 @@
   # print the list to the console [can you use one of your other methods here?]
 
   def create_list(items_string, quantity = 1)
-    item_and_num = {}
+    list = {}
     items = items_string.split(" ")
     items.each do |item|
-      item_and_num[item] = quantity
+      list[item] = quantity
     end
-    item_and_num
+    list
   end
 # output: [what data type goes here, array or hash?]
 p create_list("monkey snake cat dog")
@@ -40,19 +40,57 @@ p add_item(vegetables, "tomato")
 
 p add_item(vegetables, "potato", 2)
 
-
-
 # Method to remove an item from the list
-# input:
+# input: list, item name, and optional quantity
 # steps:
-# output:
+  # If the item_name is not the list or if the quantity is 0
+    # return 'No item to remove.'
+  # If the item name does exist, substract 1 to the quantity
+# output: print list
+
+def remove_item(list, item_name, quantity)
+  if !list.include?(item_name) || list[item_name] == 0
+    puts "No item to remove."
+  else
+    if list[item_name] < quantity
+      puts "You only have #{list[item_name]} #{item_name}."
+
+    else
+    list[item_name] -= quantity
+    end
+    list
+  end
+end
+
+p remove_item(vegetables, "onion", 2)
+p remove_item(vegetables, "apple", 1)
+p remove_item(vegetables, "potato", 2)
+p remove_item(vegetables, "potato", 1)
 
 # Method to update the quantity of an item
-# input:
+# input: list, item and quantity
 # steps:
-# output:
+  # Set the quantity to the item
+# output: print list
+def update_item(list, item_name, quantity)
+  list[item_name] = quantity
+  list
+end
+
+p update_item(vegetables, "potato", 100)
+puts update_item(vegetables, "eggplan", 5)
 
 # Method to print a list and make it look pretty
-# input:
+# input: Hash list.
 # steps:
-# output:
+  # Loop through each key-value pair and print out. each key-value pair on each line.
+# output: Each key-value pair on a line.
+def print(list)
+  list.each do |item, number|
+    puts "#{item}: #{number}"
+  end
+end
+
+print(vegetables)
+
+
