d75b73cc05b75f9e6cdf8ba4d105bcbc398cc0e0
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-03 21:07:27 -0700

    test and update the code to meet the assignment requirements

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,96 +1,117 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # [fill in any steps here]
-  # set default quantity
+  # Define a creat_list method and take two parameters -  string & quantity, set default quantity as 0.
   # print the list to the console [can you use one of your other methods here?]
 
-  def create_list(items_string, quantity = 1)
-    list = {}
-    items = items_string.split(" ")
-    items.each do |item|
-      list[item] = quantity
-    end
-    list
+def create_list(items_string, quantity = 0)
+  list = {}
+  items = items_string.split(" ")
+  items.each do |item|
+    list[item] = quantity
   end
-# output: [what data type goes here, array or hash?]
-p create_list("monkey snake cat dog")
-p create_list("flower tree grass", 3)
+  list
+end
+
+# output: return a list with hash data structure
+
+puts create_list("flower tree grass", 3)
 
 # Method to add an item to a list
-# input: list, item name, and optional quantity
+# input: list, item, and optional quantity
 # steps:
-  # If the item_name is not the list
-  # add item name and quantity as key-value pair to the list
-  # If the item name is exist, plus value one to the quantity
-
-# output: print list
+  # IF iteam does exist, set item name equal optional quantity, if optional quantity is not input, default is 1.
+  # IF the item name does exist, plus optional quantity to item, if optional quantity is not input, default is 1.
 
 def add_item(list, item_name, quantity = 1)
   if list.include?(item_name)
-    list[item_name] += 1
+    list[item_name] += quantity
   else
     list[item_name] = quantity
   end
   list
 end
 
-vegetables = create_list("tomato onion pepper")
-p add_item(vegetables, "tomato")
+# output: return a list with hash data structure
 
-p add_item(vegetables, "potato", 2)
+grocery_list = create_list("Lemonade Tomatoes Onions Ice_Cream")
+add_item(grocery_list, "Lemonade", 2)
+add_item(grocery_list, "Tomatoes", 3)
+add_item(grocery_list, "Onions", 1)
+add_item(grocery_list, "Ice_Cream", 4)
+
+puts grocery_list
 
 # Method to remove an item from the list
+# input: list and the item want to be removed
+# steps:
+  # IF there is a item in the list which is the same from the remove_item, delete it.
+
+def remove_item(list, remove_item)
+  list.delete_if { |item| item == remove_item }
+  list
+end
+
+# output: return a list with hash data structure
+
+puts remove_item(grocery_list, "Lemonade")
+
+=begin
+# Method to remove quantity of the items
 # input: list, item name, and optional quantity
 # steps:
-  # If the item_name is not the list or if the quantity is 0
+  # IF the item_name is not the list or if the quantity is 0
     # return 'No item to remove.'
-  # If the item name does exist, substract 1 to the quantity
+  # IF the item name does exist, substract 1 to the quantity
 # output: print list
 
-def remove_item(list, item_name, quantity)
-  if !list.include?(item_name) || list[item_name] == 0
-    puts "No item to remove."
+def remove_item_2(list, item_name, quantity)
+  IF !list.include?(item_name) || list[item_name] == 0
+    "No item to remove."
   else
-    if list[item_name] < quantity
-      puts "You only have #{list[item_name]} #{item_name}."
-
+    IF list[item_name] < quantity
+      "You only have #{list[item_name]} #{item_name}."
     else
-    list[item_name] -= quantity
+      list[item_name] -= quantity
+      list
     end
-    list
   end
 end
 
-p remove_item(vegetables, "onion", 2)
-p remove_item(vegetables, "apple", 1)
-p remove_item(vegetables, "potato", 2)
-p remove_item(vegetables, "potato", 1)
+puts remove_item_2(vegetables, "onion", 2)
+puts remove_item_2(vegetables, "apple", 1)
+puts remove_item_2(vegetables, "potato", 1)
+=end
 
 # Method to update the quantity of an item
 # input: list, item and quantity
 # steps:
-  # Set the quantity to the item
-# output: print list
+  # Set the item equal the input quantity to update it
+
 def update_item(list, item_name, quantity)
   list[item_name] = quantity
   list
 end
 
-p update_item(vegetables, "potato", 100)
-puts update_item(vegetables, "eggplan", 5)
+# output: return a list with hash data structure
+
+puts update_item(grocery_list, "Ice_Cream", 1)
 
 # Method to print a list and make it look pretty
 # input: Hash list.
 # steps:
-  # Loop through each key-value pair and print out. each key-value pair on each line.
-# output: Each key-value pair on a line.
+  # Loop through each key-value pair and print out each key-value pair on each line.
 def print(list)
   list.each do |item, number|
-    puts "#{item}: #{number}"
+    puts "Need to purchase: #{item} -- #{number}"
   end
 end
 
-print(vegetables)
+# output: Each key-value pair on a line.
 
+print(grocery_list)
+
+# Test for printing the result of method creat_list
+
+print(create_list("book pen ink paper"))
 
