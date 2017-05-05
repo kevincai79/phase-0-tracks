9d74c567564e03186d17334b454f2dc7e39d64a5
Eleanor Roosevelt <kevincai@YUANWENs-MacBook-Pro.local>
2017-05-04 22:19:25 -0700

    add code for release 4, complete 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+=begin
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,4 +12,46 @@ end
 
 puts Shout.yell_angrily('No')
 puts Shout.yelling_happily('Hurray')
+
+=end
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "! " + words + "! " + words + "!"
+  end
+end
+
+class Student
+  include Shout
+
+  def study
+    "Studys hard at the libriary."
+  end
+end
+
+class Actor
+  include Shout
+
+  def act(character)
+    "Playing #{character} role."
+  end
+end
+
+student = Student.new
+puts "Student class mixin:"
+puts student.yell_angrily("NO")
+puts student.yelling_happily("Yay")
+puts student.study
+puts "--------------------------------------"
+puts "Actor class mixin:"
+actor = Actor.new
+puts actor.yell_angrily("Damn")
+puts actor.yelling_happily("Hurray")
+puts actor.act("doctor")
+
+
 
