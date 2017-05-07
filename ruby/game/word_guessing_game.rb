59840f1503d9da4a66a5646ef5a3e6e701a32eb9
Eleanor Roosevelt <kevincai@YUANWENs-MacBook-Pro.local>
2017-05-07 16:46:16 -0700

    complete code for 6.7

--- ruby/game/word_guessing_game.rb
+++ ruby/game/word_guessing_game.rb
@@ -1,10 +1,8 @@
+# class
 class WordGuessingGame
-  attr_reader :is_over
 
   def initialize(phrase)
     @phrase = phrase
-    @valid_input = false
-    @is_over = false
   end
 
   def count
@@ -45,5 +43,56 @@ class WordGuessingGame
     end
     complete_phrase
   end
+end
 
+# USER INTERFACE
+
+puts "Please inter a word/phrase"
+phrase = gets.chomp.downcase
+game = WordGuessingGame.new(phrase)
+
+puts "Guess what characters are in below '_' spaces, you have maximum #{game.count} times to guess."
+puts game.original_state
+
+update_state = game.original_state
+guess_characters = []
+index = []
+i = 0
+
+while i < game.count
+
+    puts "Enter what you guess, each time with one character"
+    guess_character = gets.chomp.downcase
+
+    if !guess_characters.include?(guess_character)
+
+      guess_characters << guess_character
+      index = game.char_index(guess_character)
+
+      if !index.empty?
+        index.each do |i|
+          update_state[ i * 2 ] = guess_character
+        end
+        puts update_state
+          if update_state == game.complete_state
+            i = game.count
+            update_state
+          else
+            i += 1
+            update_state
+          end
+      else
+        i += 1
+        puts update_state
+        update_state
+      end
+    else
+      puts update_state
+    end
+end
+
+if update_state == game.complete_state
+  puts "Congratulations! You guessed it right!"
+else
+  puts "Too bad you could not guess it :("
 end
