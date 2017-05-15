07aca62495f1899c2c76850f648a5c27883657e1
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-15 01:58:48 -0700

    add code for release 2

--- js/algos.js
+++ js/algos.js
@@ -67,6 +67,33 @@ function compareObject(obj1, obj2) {
   }
 }
 
+// RELEASE 2
+// Set an empty array randomStrings and a variable for letters.
+// Repeat the times of parameter doing below:
+  // Set a random length for a string.
+  // Loop through the random length times doing below:
+    // Set a random index for string.
+    // Add all the random letters together to be a random string.
+  // Add the random string to the randomStrings array.
+// Return the value of randomStrings array.
+
+function randomString(int) {
+  var randomStrings = [];
+  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  for (var i = 0; i < int; i++) {
+    var randomLength = Math.floor(Math.random() * 10 +1);
+    var randomString = "";
+
+    for (var j = 0; j < randomLength; j++) {
+      var randomIndex = Math.floor(Math.random() * 52);
+      randomString += letters.charAt(randomIndex);
+    }
+    randomStrings.push(randomString);
+  }
+  return randomStrings;
+}
+
 // Test code for release 0:
 // var a = ["a", "hello wo", "sea world", "hi", "hellow world", "oala", "15"];
 // var b = ["long phrase","longest phrase","longer phrase"];
@@ -76,9 +103,13 @@ function compareObject(obj1, obj2) {
 // console.log(longestPhrase(c));
 
 //Test code for release 1:
-console.log(compareObject({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
-console.log(compareObject({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
-console.log(compareObject({animal: "Dog", legs: 4}, {country: "US", tree: 3}));
-console.log(compareObject({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3, age: 7}));
+// console.log(compareObject({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+// console.log(compareObject({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+// console.log(compareObject({animal: "Dog", legs: 4}, {country: "US", tree: 3}));
+// console.log(compareObject({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3, age: 7}));
 
+// Test code for release 2:
+// console.log(randomString(5));
+// console.log(randomString(3));
+// console.log(randomString(7));
 
