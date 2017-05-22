4bcd68ef02a125f1026963c18510f83093bc7845
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-21 21:17:26 -0700

    create original student database

--- databases/student_data/student_data.rb
+++ databases/student_data/student_data.rb
@@ -9,7 +9,7 @@ require 'faker'
 
 db = SQLite3::Database.new("students.db")
 
-# Add methods for updating, deleting and add students.
+# Create students table.
 
 create_table = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
@@ -22,7 +22,25 @@ create_table = <<-SQL
 
 db.execute(create_table)
 
-# Create SQLite database.
+# Add methods for adding, updating, deleting students from the data.
+
+def add_student(db, first_name, last_name, age)
+  db.execute('INSERT INTO students (first_name, last_name, age) VALUES (?, ?, ?)', [first_name, last_name, age])
+end
+
+def remove_student(db, int)
+  db.execute('DELETE FROM students WHERE id = ?', int)
+end
+
+# Create original students SQLite database.
+
+age = [20, 21, 22, 23, 24, 25]
+
+20.times do
+  add_student(db, Faker::Name.first_name, Faker::Name.last_name, age.sample)
+end
+
+
 
 # USER INTERFACE
 
