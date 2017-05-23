f7c3fb3025af27887198df8ea61a82f470a2ace2
Eleanor Roosevelt <kevincai@yuanwens-mbp.gateway.sonic.net>
2017-05-23 15:14:22 -0700

    add code for release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,10 @@
 console.log("Script is running");
 var header = document.getElementById("header");
 header.style.border = "1px solid blue";
+
+function addBorder(event) {
+  event.target.style.border = "3px dotted orange";
+}
+
+var photo = document.getElementById("smiley-face");
+photo.addEventListener("click", addBorder);
